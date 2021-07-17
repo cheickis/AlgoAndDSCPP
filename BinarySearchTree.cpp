924084fd2cc267e1ac4c7f0650e37bc03053d5df
cheickis <cheick.ismo@gmail.com>
2021-07-17 14:58:25 -0300

    add sum function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,1 +1,221 @@
 #include "BinarySearchTree.h"
+
+int BinarySearchTree::lookup(struct Node* node,int target)
+{
+	if (node == NULL)
+		 return 0;
+	if (node->data == target) {
+		return true;
+    if (target < node->data)
+			return lookup(node->left, target);
+     else
+			return lookup(node->right, target);
+	}	
+}
+
+void BinarySearchTree::insert(Node* rootNode, int val)
+{
+	Node* newNode = NewNode(val);
+
+	if (rootNode == NULL) {
+		rootNode = newNode;
+	}
+	else if (val <= rootNode->data) {
+		insert(rootNode->left, val);
+	}
+	else {
+		insert(rootNode->right, val);
+	}
+}
+
+
+int BinarySearchTree::size(Node * root) {
+
+	if (root == NULL) {
+		return 0;
+	}
+	else {
+		return 1 + size(root->left) + size(root->right);
+	}
+}
+
+int BinarySearchTree::maxDepth(Node* root) {
+	if (root == NULL) {
+		return 0;
+	}
+	else {
+
+		int lmaxDepth = maxDepth(root->left);
+		int rmaxDepth = maxDepth(root->right);
+		if (lmaxDepth > rmaxDepth) return 1 + lmaxDepth;
+		else
+			return 1 + rmaxDepth;
+
+	}
+
+}
+/*
+Given a non-empty binary search tree,
+return the minimum data value found in that tree.
+*/
+int BinarySearchTree::minValue(Node* root)
+{
+	Node* curr = root;
+	while (curr != NULL) {
+	
+		curr = curr->left;
+	
+	}
+	if (curr!=NULL) {
+		return curr->data;
+	}
+	
+	return 0;
+}
+
+int BinarySearchTree::maxValue(Node* root)
+{
+	Node* curr = root;
+	while (curr != NULL) {
+
+		curr = curr->right;
+
+	}
+	if (curr != NULL) {
+		return curr->data;
+	}
+
+
+	return 0;
+}
+/*
+Given a binary search tree, print out
+its data elements in increasing
+sorted order.
+*/
+void BinarySearchTree::printTree(Node* root)
+{
+	if (root == NULL) return;
+	printTree(root->left);
+	std::cout << root->data;
+	printTree(root->right);
+}
+/*
+Given a binary tree, print its
+nodes according to the "bottom-up"
+postorder traversal.
+*/
+void BinarySearchTree::printPostOrder(Node* root)
+{
+	if (root == NULL) return;
+	printPostOrder(root->left);
+	printPostOrder(root->right);
+	std::cout << root->data;
+}
+
+void BinarySearchTree::printInorder(Node* root)
+{
+	if (root == NULL) return;
+	printInorder(root->left);
+    std::cout << root->data;
+	printInorder(root->right);
+}
+
+void BinarySearchTree::printPreOrder(Node* root)
+{
+	if (root == NULL) return;
+    std::cout << root->data;
+	printPreOrder(root->left);
+	printPreOrder(root->right);
+
+}
+
+/*
+Given a tree and a sum, return true if there is a path from the root
+down to a leaf, such that adding up all the values along the path
+equals the given sum.
+*/
+int BinarySearchTree::hashPathSum(Node* root, int sum)
+{
+	if (root == NULL) {
+		return (sum == 0);
+	}
+	else {
+
+		int subSum = sum - root->data;
+		return hashPathSum(root->left, subSum) || hashPathSum(root->right, subSum);
+	}
+
+}
+/*
+Given a binary tree, print out all of its root-to-leaf
+paths, one per line. Uses a recursive helper to do the work.
+*/
+void BinarySearchTree::printPaths()
+{
+}
+
+void printPathsHelper(Node* node, int path[], int pathlen) {
+	if (node == NULL) return;
+	path[pathlen] = node->data;
+	pathlen++;
+
+	if (node->left == NULL && node->right == NULL) {
+
+		printArray(path, pathlen);
+	}
+	else {
+		printPathsHelper(node->left, path, pathlen);
+		printPathsHelper(node->right, path, pathlen);
+
+	}
+
+
+}
+
+void printArray(int ints[], int len) {
+	for (int i =0 ; i < len; i++) {
+	
+		std::cout << ints[i];
+	}
+
+}
+/*
+Change a tree so that the roles of the
+left and right pointers are swapped at every node.
+So the tree...
+			4
+		   / \
+		  2   5
+		 / \
+		1   3
+is changed to...
+			4
+			/ \
+			5 2
+			/ \
+			3 1
+
+*/
+bool BinarySearchTree::mirror()
+{
+	return false;
+}
+
+void BinarySearchTree::doubleTree()
+{
+}
+
+void BinarySearchTree::sameTree()
+{
+}
+
+int BinarySearchTree::countTrees()
+{
+	return 0;
+}
+
+bool BinarySearchTree::isBST()
+{
+	return false;
+}
--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -1,41 +1,84 @@
 #pragma once
+#include <iostream>
 #include <cstddef>
-struct node {
+#include <math.h> 
+#include <queue>
+typedef struct Node {
 	int data;
-	struct node* left;
-	struct node* right;
-};
+	struct Node* left;
+	struct Node* right;
+}node;
 class BinarySearchTree
 {
 private:
-	node root;
+	Node root;
 	
 
  public :
 
-	 struct node* NewNode(int data) {
-		 struct node* node = new(struct node); // "new" is like "malloc"
+	 Node* NewNode(int data) {
+		 Node* node = new(Node); // "new" is like "malloc"
 		 node->data = data;
 		 node->left = NULL;
 		 node->right = NULL;
 		 return(node);
 	 }
-	 int lookup(int targert);
-	 void inser(int val);
-	 int size ();
-	 int maxDepth();
-	 int minValue();
-		 void printTree();
-		 void printPostOrder();
-		 void printInorder();
-		 void printPretPost();
-		 void hashPathSum();
-		 void printPaths();
-		 bool mirror();
-		 void doubleTree();
-		 void sameTree();
-		 int countTrees();
-		 bool isBST();
+	 /*
+		Given a binary tree, return true if a node
+		with the target data is found in the tree..
+      */
+	 int  lookup(Node* node,int targert);
+	 /*
+		Give a binary search tree and a number, inserts a new node
+		with the given number in the correct place in the tree.
+		Returns the new root pointer which the caller should
+		then use (the standard trick to avoid using reference parameters).
+     */
+	 void insert(Node* root, int val);
+	 /*
+	 traversal. Given a binary tree, count the number of nodes in the tree.
+	 */
+	 int  size (Node * root);
+
+	 /*
+	 Given a binary tree, compute its "maxDepth" -- the number of nodes along the longest path from the root node down
+		to the farthest leaf node. The maxDepth of the empty tree is 0,
+	 */
+	 int  maxDepth(Node *root);
+	 /*
+	 * Given a non-empty binary search tree (an ordered binary tree), return the minimum data value found in that tree.
+     Note that it is not necessary to search the entire tree. 
+	 */
+	 int  minValue(Node *root);
+
+	 /**
+	 Given a non-empty binary search tree (an ordered binary tree), return the maximum data value found in that tree.
+      Note that it is not necessary to search the entire tree.
+	 **/	 
+	 
+	 int maxValue(Node* root);
+	 /*
+	 Given a binary search tree (aka an "ordered binary tree"), iterate over the nodes to print them out in increasing
+	 order. So the tree...
+				4
+			   / \
+			  2   5
+			 / \
+            1   3
+     Produces the output "1 2 3 4 5". This is known as an "inorder" traversal of the tree.
+	 */
+	 void printTree(Node *root);
+	 void printPostOrder(Node *root);
+	 void printInorder(Node *root);
+     void printPreOrder(Node *root);
+
+     int hashPathSum(Node *root, int sum);
+     void printPaths();
+	 bool mirror();
+     void doubleTree();
+	 void sameTree();
+	 int  countTrees();
+     bool isBST();
 
 
 };
